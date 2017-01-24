927edbb06b8a112a67793a9c6d006be0aa7446d8
Andrey Kasatkin <a@svetliy.com>
2017-01-24 11:58:45 -0500

    missing method

--- Zype/Views/URLImageView.swift
+++ Zype/Views/URLImageView.swift
@@ -51,6 +51,10 @@ class URLImageView: UIImageView {
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
   }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
   
   override func layoutSubviews() {
     super.layoutSubviews()
