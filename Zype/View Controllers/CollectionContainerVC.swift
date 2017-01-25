a1d8e96863cbff2db09aad771e48f126e159d417
Andrey Kasatkin <a@svetliy.com>
2017-01-25 10:48:01 -0500

    add logic for loading image thumbnail for enhanced playlists

--- Zype/View Controllers/CollectionContainerVC.swift
+++ Zype/View Controllers/CollectionContainerVC.swift
@@ -73,11 +73,18 @@ class PagerCollectionItem: CollectionLabeledItem {
 
 
 class ShowCollectionItem: CollectionLabeledItem {
-  init(value: PlaylistModel) {
-    super.init()
-    self.title = value.titleString
-    self.object = value
-  }
+    init(value: PlaylistModel) {
+        super.init()
+        self.title = value.titleString
+        if(value.images.count > 0) {
+            self.imageURL = NSURL(string: value.images.first!.imageURL) as URL!
+        } else if (value.thumbnails.count > 0) {
+            self.imageURL = NSURL(string: value.thumbnails.first!.imageURL) as URL!
+        } else {
+            self.imageURL = NSURL(string: "http://placehold.it/320x180") as URL!
+        }
+        self.object = value
+    }
 }
 
 extension UIViewController {
