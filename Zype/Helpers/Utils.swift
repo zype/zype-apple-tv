b72aeacf31320ae3776389dd7133337ebef99a04
Alexander Bugaev <gryzzlys@gmail.com>
2017-10-31 17:10:17 +0600

    kParentalGuidanceProtection added

--- Zype/Helpers/Utils.swift
+++ Zype/Helpers/Utils.swift
@@ -85,3 +85,16 @@ func isValidEmail(_ email:String) -> Bool {
   let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
   return emailTest.evaluate(with: email)
 }
+
+extension UIView {
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 3
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 10, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 10, y: self.center.y))
+        self.layer.add(animation, forKey: "position")
+    }
+}
+
--- Zype/View Controllers/PurchaseVC.swift
+++ Zype/View Controllers/PurchaseVC.swift
@@ -11,6 +11,13 @@ import UIKit
 class PurchaseVC: UIViewController {
     
     @IBOutlet var containerView: UIView!
+    @IBOutlet var attemptView: UIView!
+    @IBOutlet var attemptInputView: UITextField!
+    @IBOutlet var attemptDecriptionLabel: UILabel!
+    
+    var result: Int = 0
+    var productIDSelected: String!
+
     var scrollView: UIScrollView!
     var stackView: UIStackView!
     
@@ -33,10 +40,26 @@ class PurchaseVC: UIViewController {
     
     func onPlanSelected(sender: UIButton) {
         if let identifier = sender.accessibilityIdentifier {
-            self.purchase(identifier)
+            if Const.kParentalGuidanceProtection == true {
+                self.showAttemptView(identifier: identifier)
+            } else {
+                self.purchase(identifier)
+            }
         }
         print("\n\n HELLO PURCHASE WORLD \n\n")
         print(sender.tag)
+    }
+    
+    @IBAction func onAttemptCancelSelected(_ sender: Any) {
+        self.hideAttemptView()
+    }
+    
+    @IBAction func onAttemptCompleteSelected(_ sender: Any) {
+        if "\(self.result)" == self.attemptInputView.text {
+            self.attemptCorrectSelected()
+        } else {
+            self.attemptWrongSelected()
+        }
     }
     
     func purchase(_ productID: String) {
@@ -74,4 +97,55 @@ class PurchaseVC: UIViewController {
             }
         }
     }
+    
+    // MARK - Attempt View
+    
+    func calculateRandomNumbers() -> String {
+        let firstNumber = Int(arc4random_uniform(9)) + 1
+        let secondNumber = Int(arc4random_uniform(9)) + 1
+        self.result = firstNumber + secondNumber
+        return "How much will \(firstNumber) + \(secondNumber)?"
+    }
+    
+    func showAttemptView(identifier: String) {
+        self.attemptInputView.text = ""
+        self.attemptInputView.setNeedsFocusUpdate()
+        self.productIDSelected = identifier
+        self.attemptDecriptionLabel.text = self.calculateRandomNumbers()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.containerView.alpha = 0
+            self.attemptView.alpha = 1
+        }) { (complete) in
+            self.containerView.isHidden = true
+            self.attemptView.isHidden = false
+            self.containerView.alpha = 1
+            self.attemptView.alpha = 1
+        }
+    }
+    
+    func hideAttemptView() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.containerView.alpha = 1
+            self.attemptView.alpha = 0
+        }) { (complete) in
+            self.containerView.isHidden = false
+            self.attemptView.isHidden = true
+            self.containerView.alpha = 1
+            self.attemptView.alpha = 1
+        }
+    }
+    
+    func attemptWrongSelected() {
+        self.attemptInputView.text = ""
+        self.attemptInputView.shake()
+    }
+    
+    func attemptCorrectSelected() {
+        self.attemptInputView.text = ""
+        self.hideAttemptView()
+        if self.productIDSelected != nil {
+            self.purchase(self.productIDSelected)
+        }
+    }
+    
 }
