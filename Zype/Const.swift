68825d74f126a30ef0a8a3afd1de914407ca7aeb
Manish <praveshkumar@Praveshs-MacBook-Air.local>
2020-03-03 02:13:00 +0530

    Disabled Segment Analytics by default

--- Zype/Const.swift
+++ Zype/Const.swift
@@ -50,9 +50,9 @@ class Const: NSObject {
     static let kLiveItemEnabled = false
     static let kLiveVideoID = "5c8faa021d1f4314dd006203"
     static let kEpisodeNumberDisplay = false
-    static let kSegmentAnalytics = true // if enabled,make sure kSegmentAccountID is having correct value
+    static let kSegmentAnalytics = false // if enabled,make sure kSegmentAccountID is having correct value
     static let kSegmentAnalyticsWriteKey = "N1ybwfgbQz2nSTPhVfKPAS8Py4jV6Agd"
-    static let kSegmentAccountID = "xyz" // must have some value if kSegmentAnalytics is enabled
+    static let kSegmentAccountID = "" // must have some value if kSegmentAnalytics is enabled
 
     // NOTE: This is a deprecated feature. DO NOT ENABLE
     static let kNativeToUniversal = false
